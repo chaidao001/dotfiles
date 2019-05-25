447894f068a36b797d1e1db3131bf82be22f471d
chaidao001 <chaidao0001@gmail.com>
2019-05-25 10:17:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ ln -s ~/dotfiles/.vimrc ~/
 ln -s ~/dotfiles/.tmux.conf ~/
 ln -s ~/dotfiles/.vim ~/
 ln -s ~/dotfiles/.ansible.cfg ~/
-ln -s ~/dotfiles/ssh/config ~/.ssh/
 ln -s ~/dotfiles/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
 ln -s ~/dotfiles/karabiner/karabiner.json ~/.config/karabiner
 ln -s ~/dotfiles/mac/com.apple.symbolichotkeys.plist ~/Library/Preferences
